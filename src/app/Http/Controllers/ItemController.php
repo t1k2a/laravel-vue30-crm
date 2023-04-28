db2064b725537d52d88da8f079f0de43be8bc3d1
t1k2a <maxtj1122@gmail.com>
2023-04-28 23:33:25 +0900

    途中コミット

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Item;
+use App\Http\Requests\StoreItemRequest;
+
 
 class ItemController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return Inertia::render('Items/index', [
+            'items' => Item::select('id', 'name', 'price', 'is_selling')->get()
+        ]);
+    }
+
+    public function create()
+    {
+
+        return Inertia::render('Items/Create');
+    }
+
+    public function store(StoreItemRequest $request)
+    {
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+
+        return to_route('items.index');
+    }
+
+    
 }
