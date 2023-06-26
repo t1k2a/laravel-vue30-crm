392713aa3ad57c06d77cfb1902b71efa02f3a848
t1k2a <maxtj1122@gmail.com>
2023-06-26 22:11:01 +0900

    月別年別分析を追加

--- src/resources/js/Pages/Analysis.vue
+++ src/resources/js/Pages/Analysis.vue
@@ -55,6 +55,25 @@ const getData = async () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="getData">
+                            分析方法<br />
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="perDay"
+                                checked
+                            /><span class="mr-4">日別</span>
+
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="perMonth"
+                            /><span class="mr-4">月別</span>
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="perYear"
+                            /><span class="mr-4">年別</span>
+                            <br />
                             From:<input
                                 type="date"
                                 name="startDate"
